d9be7b6ad312c702cf79b6a79093866c532aa35a
Yodaorwa <yodahelena@gmail.com>
2022-10-17 07:13:09 -0700

    101 quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unsistd.h>
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *
+ *Return: Always 1.
+ */
+int main (void)
+{
+	write(1, "and thta piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
