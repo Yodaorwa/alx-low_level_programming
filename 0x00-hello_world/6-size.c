5f38ecf743cd3f4e34fc339d1d5220fd4f8c7d40
Yodaorwa <yodahelena@gmail.com>
2022-10-17 06:31:17 -0700

    printing sizes of various types corr.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- *main - Prints the size of various types based on the computer it is compiled and run on
+ *main - Prints size of various types on the computer it is compiled and run on
  *
  *Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n",sizeof(char));
-	printf("Size of an int: %zu byte(s)\n",sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n",sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
