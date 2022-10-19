06731e6cce64b8e038870b7c0d09b0cd53da2902
Yodaorwa <yodahelena@gmail.com>
2022-10-19 09:49:51 -0700

    last digit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,20 +4,22 @@
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 	{
-	int a;
+	int num;
 
-	for (a = 0; a < 10; a++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		putchar(a + '0');
-		if (a < 9)
+		putchar(num);
+		if (num == '9')
+		{
+			break;
+		}
+		else
 		{
 			putchar(',');
-			putchar(',');
+			putchar(' ');
 
 		}
 		putchar('\n');
-		return (0);
-}
+		return (0);	}
